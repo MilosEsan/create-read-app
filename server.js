95c8bc3d20743c9fe8f0c78bbdd33ae35fa5ad79
EsanMilos <milos.esan@bridgewaterlabs.com>
2020-08-20 19:43:40 +0200

    start-comm

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const WordRepository = require("./word-repo.js");
 
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 const wordRepository = new WordRepository("content/words1.json");
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-app.get("/api/word", async (req, res) => {
-  let result = await wordRepository.findWord(req.query.w);
+app.get("/api/word", async(req, res) => {
+    let result = await wordRepository.findWord(req.query.w);
 
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(result), "utf8");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(result), "utf8");
 });
 
-app.post("/api/word", async (req, res) => {
-  await wordRepository.addWord(req.body);
+app.post("/api/word", async(req, res) => {
+    await wordRepository.addWord(req.body);
 
-  res.writeHead(200);
-  res.end("word added", "utf8");
+    res.writeHead(200);
+    res.end("word added", "utf8");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`app listening on port ${port}!`));
